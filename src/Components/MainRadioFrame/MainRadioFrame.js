a44a30dff8e5404be68267c548bd6069a61dc492
avishekataur <avishekataur@gmail.com>
2022-05-20 21:53:24 +0600

    bar_design_done

--- src/Components/MainRadioFrame/MainRadioFrame.js
+++ src/Components/MainRadioFrame/MainRadioFrame.js
@@ -1,13 +1,50 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import "./MainRadioFrame.css";
-
 const MainRadioFrame = () => {
   return (
     <div className="mainRadioFrame">
       {/* Station */}
-      <div className="singleRadioFrame"></div>
+      <div className="singleRadioFrame">
+        <div className="frameBar">
+          <a href="/#" alt="">
+            <FontAwesomeIcon
+              className="mx-2 text-red-500 icon"
+              icon={faChevronLeft}
+            />
+          </a>
+          <a href="/#" alt="">
+            Station
+          </a>
+          <a href="/#" alt="">
+            <FontAwesomeIcon
+              className="mx-2 text-red-500 icon"
+              icon={faPowerOff}
+            />
+          </a>
+        </div>
+      </div>
       {/* FM Radio */}
-      <div className="singleRadioFrame"></div>
+      <div className="singleRadioFrame">
+        <div className="frameBar">
+          <a href="/#" alt="">
+            <FontAwesomeIcon
+              className="mx-2 text-red-500 icon"
+              icon={faChevronLeft}
+            />
+          </a>
+          <a href="/#" alt="">
+            FM Radio
+          </a>
+          <a href="/#" alt="">
+            <FontAwesomeIcon
+              className="mx-2 text-red-500 icon"
+              icon={faPowerOff}
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
