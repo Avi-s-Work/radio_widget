6de7c1b2646ef214cf5194d7d31027785da727c1
avishekataur <avishekataur@gmail.com>
2022-05-21 00:55:27 +0600

    circle

--- src/Components/MainRadioFrame/MainRadioFrame.js
+++ src/Components/MainRadioFrame/MainRadioFrame.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import Wave from "../../imgs/wave.png";
 import "./MainRadioFrame.css";
 const MainRadioFrame = () => {
   return (
@@ -93,6 +94,7 @@ const MainRadioFrame = () => {
       </div>
       {/* //////////////////// FM Radio ////////////////////// */}
       <div className="singleRadioFrame">
+        {/* Top Bar */}
         <div className="topBar">
           <a href="/#" alt="">
             <FontAwesomeIcon
@@ -110,6 +112,16 @@ const MainRadioFrame = () => {
             />
           </a>
         </div>
+        {/* Single Station */}
+        <div className="singleStation">
+        <div className="circle">
+        <h1>98.9</h1>
+        </div>
+        <div>
+          <img className="wave" src={Wave} alt="" />
+        </div>
+        </div>
+        {/* Bottom Bar  */}
         <div className="bottomBar">
           <div>
             <p>Now Playing</p>
