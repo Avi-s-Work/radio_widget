dfce33959d3005f898e2ce974c88d06127c8bc0f
avishekataur <avishekataur@gmail.com>
2022-05-22 06:53:50 +0600

    done

--- src/Components/MainRadioFrame/MainRadioFrame.js
+++ src/Components/MainRadioFrame/MainRadioFrame.js
@@ -6,12 +6,20 @@ import "./MainRadioFrame.css";
 
 const MainRadioFrame = () => {
   const [stations, setStations] = useState([]);
+  //Selected Station
+  const [selectedStation, SetSelectedStation] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/stations")
       .then((res) => res.json())
       .then((data) => setStations(data));
   }, []);
+
+  //Select Station By Click
+  const clickSingleStation = (station) => {
+    const singleStaion = [station];
+    SetSelectedStation(singleStaion);
+  };
 
   return (
     <div className="mainRadioFrame">
@@ -39,7 +47,7 @@ const MainRadioFrame = () => {
         <div className="stationNameList">
           {stations.map((station) => (
             <div key={station._id} station={station} className="stationName">
-              <a href="/#" alt="">
+              <a href="/#" alt="" onClick={() => clickSingleStation(station)}>
                 <h2>{station.stationName}</h2>
                 <h2>{station.frequency}</h2>
               </a>
@@ -50,41 +58,45 @@ const MainRadioFrame = () => {
         <div className="bottomBar"></div>
       </div>
       {/* //////////////////// FM Radio ////////////////////// */}
-      <div className="singleRadioFrame">
-        {/* Top Bar */}
-        <div className="topBar">
-          <a href="/#" alt="">
-            <FontAwesomeIcon
-              className="mx-2 text-red-500 icon"
-              icon={faChevronLeft}
-            />
-          </a>
-          <a href="/#" alt="">
-            FM Radio
-          </a>
-          <a href="/#" alt="">
-            <FontAwesomeIcon
-              className="mx-2 text-red-500 icon"
-              icon={faPowerOff}
-            />
-          </a>
-        </div>
-        {/* Single Station */}
-        <div className="singleStation">
-          <div className="circle">
-            <h1>98.9</h1>
+      <div>
+        {selectedStation?.map((singleStaions) => (
+          <div key={singleStaions._id} className="singleRadioFrame">
+            {/* Top Bar */}
+            <div className="topBar">
+              <a href="/#" alt="">
+                <FontAwesomeIcon
+                  className="mx-2 text-red-500 icon"
+                  icon={faChevronLeft}
+                />
+              </a>
+              <a href="/#" alt="">
+                FM Radio
+              </a>
+              <a href="/#" alt="">
+                <FontAwesomeIcon
+                  className="mx-2 text-red-500 icon"
+                  icon={faPowerOff}
+                />
+              </a>
+            </div>
+            {/* Single Station */}
+            <div className="singleStation">
+              <div className="circle">
+                <h1>{singleStaions.frequency}</h1>
+              </div>
+              <div>
+                <img className="wave" src={Wave} alt="" />
+              </div>
+            </div>
+            {/* Bottom Bar  */}
+            <div className="bottomBar">
+              <div>
+                <p>Currently Playing</p>
+                <h3>{singleStaions.stationName}</h3>
+              </div>
+            </div>
           </div>
-          <div>
-            <img className="wave" src={Wave} alt="" />
-          </div>
-        </div>
-        {/* Bottom Bar  */}
-        <div className="bottomBar">
-          <div>
-            <p>Now Playing</p>
-            <h3>BHOOT FM</h3>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
